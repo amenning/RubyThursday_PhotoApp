6a6f59af5b817abb426d8b5f0de599d1034faf8a
Andrew Menning <menning23@gmail.com>
2017-07-09 12:31:46 -0400

    Member can create new group

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170708231423) do
+ActiveRecord::Schema.define(version: 20170709144808) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20170708231423) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["member_id"], name: "index_albums_on_member_id"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "member_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["member_id"], name: "index_groups_on_member_id"
   end
 
   create_table "images", force: :cascade do |t|
