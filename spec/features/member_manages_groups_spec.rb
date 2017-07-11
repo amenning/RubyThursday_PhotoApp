0f44c36fd0e932d86ae636ea87ab22508b0cb4a5
Andrew Menning <menning23@gmail.com>
2017-07-11 19:27:14 -0400

    Refactor group creation to only include name

--- spec/features/member_manages_groups_spec.rb
+++ spec/features/member_manages_groups_spec.rb
@@ -4,7 +4,7 @@ feature 'member manages groups' do
   let!(:member) { FactoryGirl.create(:member) }
   let!(:member2) { FactoryGirl.create(:member, first_name: 'Todd', last_name: 'Wahnish') }
 
-  scenario 'by creating a new group', js: true do
+  scenario 'by creating a new group' do
     login_as(member, scope: :member)
     visit root_path
     click_link 'Dashboard'
@@ -14,7 +14,30 @@ feature 'member manages groups' do
     expect(page).to have_content 'Create A New Group!'
 
     fill_in 'group[name]', with: 'The Wahnishes'
-    select 'Todd'
+    click_button 'Create group'
+    expect(page).to have_content 'Group was successfully created.'
+    expect(page).to have_content 'The Wahnishes'
+    expect(page).to have_content 'Add Members'
+    expect(Group.count).to eq 1
+
+    group = Group.last
+    expect(group).to have_attributes(
+      name: 'The Wahnishes',
+      member_id: member.id
+    )
+  end
+
+  scenario 'by creating a new group and inviting a non-member' do
+    login_as(member, scope: :member)
+    visit root_path
+    click_link 'Dashboard'
+    expect(page).to have_content 'My Dashboard'
+
+    click_link 'Create New Group'
+    expect(page).to have_content 'Create A New Group!'
+
+    fill_in 'group[name]', with: 'The Wahnishes'
+    fill_in 'new_member_email', with: 'melissa@rubythursday.com'
     click_button 'Create group'
     expect(page).to have_content 'Group was successfully created.'
     expect(page).to have_content 'The Wahnishes'
@@ -27,10 +50,8 @@ feature 'member manages groups' do
       member_id: member.id
     )
 
-    group_member = GroupMember.last
-    expect(group_member).to have_attributes(
-      group_id: group.id,
-      member_id: member2.id
-    )
+    new_member = Member.last
+    expect(new_member.email).to eq 'melissa@rubythursday.com'
+    expect(ActionMailer::Base.deliveries.count).to eq(1)
   end
 end
