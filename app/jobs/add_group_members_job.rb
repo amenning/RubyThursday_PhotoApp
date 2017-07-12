1b85e345509dc3d45c531966fe0b4f9427f3241e
Andrew Menning <menning23@gmail.com>
2017-07-11 20:48:07 -0400

    Add group members job creates association for existing members

--- app/jobs/add_group_members_job.rb
+++ app/jobs/add_group_members_job.rb
@@ -1,7 +1,12 @@
 class AddGroupMembersJob < ApplicationJob
-  queue_as :default
+  queue_as :add_group_members
 
-  def perform(*args)
-    # Do something later
+  def perform(new_members_emails, group)
+    email_array = new_members_emails.split(',')
+    email_array.each do |email|
+      clean_email = email.strip
+      member = Member.find_by_email(clean_email)
+      GroupMember.create(group: group, member: member)
+    end
   end
 end
--- spec/jobs/add_group_members_job_spec.rb
+++ spec/jobs/add_group_members_job_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe AddGroupMembersJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:member2) do
+    FactoryGirl.create(
+      :member,
+      first_name: 'Todd',
+      last_name: 'Wahnish',
+      email: 'todd@rubythursday.com'
+    )
+  end
+  let!(:group) { FactoryGirl.create(:group) }
+  describe 'for existing member' do
+    it 'creates group membership association' do
+      new_members_emails = 'todd@rubythursday.com'
+      expect { AddGroupMembersJob.perform_now(new_members_emails, group) }
+        .to change { GroupMember.count }.from(0).to(1)
+    end
+  end
+
+  # describe 'for non members' do
+  #   it 'creates member' do
+  #
+  #   end
+  #
+  #   it 'sends invitation' do
+  #
+  #   end
+  #
+  #   it 'creates group membership associations' do
+  #
+  #   end
+  # end
 end
